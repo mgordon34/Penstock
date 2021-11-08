f04af9d8aae69b7f1af1841a289f6c66ee0b4441
Matt Gordon <coltfanman@gmail.com>
2021-11-08 01:08:40 -0500

    Renamed template db file, created reference over explicit name in tests

--- tests/utils.py
+++ tests/utils.py
@@ -2,12 +2,13 @@ import os
 import shutil
 import sqlite3
 
+TEMPLATE_FILE = 'template_data.db'
 DB_FILE = 'test_data.db'
 
 def setup_test_db():
     print('copying db')
     db_file = DB_FILE
-    shutil.copy('data_template.db', db_file)
+    shutil.copy(TEMPLATE_FILE, db_file)
     return db_file
 
 def teardown_test_db():
