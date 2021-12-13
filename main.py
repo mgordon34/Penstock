e4ac6e8cb1eb9c3d980114a4a6a6b1efa78bea61
Matt Gordon <coltfanman@gmail.com>
2021-12-12 23:03:35 -0600

    first pass with flask api server

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from ta.strategies import HistoricalThreeBarStrategy, LiveThreeBarStrategy
 from portfolio import Portfolio
 import queue
 from event import *
+from  flask import Flask
 import common.config as config
 
 import logging
 log = logging.getLogger(__name__)
 logging.getLogger('urllib3').setLevel(logging.CRITICAL)
+
+app = Flask(__name__)
 
 events = queue.Queue()
 stream = None
@@ -26,32 +29,41 @@ total_winners = 0
 total_losers = 0
 balance = config.starting_balance
 
+
 # for date in config.dates:
 #   stream = HistoricalDataStreamer(events, 'bars', config.symbols, date+'T13:30:00Z', date+'T19:59:00Z')
 #   strat = ThreeBarStrategy(events, stream)
 #   portfolio = Portfolio(events, balance, config.pct_buying_power, config.max_positions)
 
-while True:
-    try:
-        stream.update_price()
-        while True:
-            try:
-                event = events.get(False)
-            except queue.Empty:
-                break
-            else:
-                if event.type == 'MARKET':
-                    strat.handle_mkt_event(event)
-                elif event.type == 'SIGNAL':
-                    portfolio.handle_signal_event(event)
-    except OutofDataError:
-      log.info('Streamer ran out of bars, finishing...')
-      result = portfolio.calculate_performance()
-      balance += result['profit']
-      total_profit += result['profit']
-      total_winners += result['winners']
-      total_losers += result['losers']
-      break
+@app.route('/')
+def hello_world():
+    return '<p>Hello, from stonks api!</p>'
 
-log.info('Profit: ${:,.2f}, {} winners and {} losers'.format(total_profit, total_winners, total_losers))
-quit()
+@app.route('/run')
+def run():
+    while True:
+        try:
+            stream.update_price()
+            while True:
+                try:
+                    event = events.get(False)
+                except queue.Empty:
+                    break
+                else:
+                    if event.type == 'MARKET':
+                        strat.handle_mkt_event(event)
+                    elif event.type == 'SIGNAL':
+                        portfolio.handle_signal_event(event)
+        except OutofDataError:
+            log.info('Streamer ran out of bars, finishing...')
+            result = portfolio.calculate_performance()
+            balance += result['profit']
+            total_profit += result['profit']
+            total_winners += result['winners']
+            total_losers += result['losers']
+            break
+
+    log.info('Profit: ${:,.2f}, {} winners and {} losers'.format(total_profit, total_winners, total_losers))
+    quit()
+
+app.run(host="0.0.0.0", port=6969)
